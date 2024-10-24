7153a385f7bbbeda3b57f8c0cc701f7297ef79fa
Ethaniel Billon <billon@ei-gv-790.creatis.insa-lyon.fr>
2024-10-24 11:41:27 +0200

    smaserver class wrapper

---
+++ sma-server/src/main/java/fr/insalyon/creatis/sma/server/SmaServer.java
@@ -0,0 +1,46 @@
+package fr.insalyon.creatis.sma.server;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import fr.insalyon.creatis.sma.common.Communication;
+import fr.insalyon.creatis.sma.server.execution.Executor;
+import fr.insalyon.creatis.sma.server.execution.MessageCleanerPool;
+
+/**
+ * SmaServer
+ */
+public class SmaServer extends Thread {
+
+    private static final Logger logger = Logger.getLogger(Main.class);
+
+    @Override
+    public void run() {
+        PropertyConfigurator.configure(Main.class.getClassLoader().getResource("smaLog4j.properties"));
+        Configuration.getInstance();
+
+        logger.info("Starting SMA Server on port " + Configuration.getInstance().getPort());
+
+        // Pools
+        MessageCleanerPool.getInstance();
+        
+        // Socket
+        try (ServerSocket serverSocket = new ServerSocket(
+            Configuration.getInstance().getPort(), 50, InetAddress.getLocalHost())) {
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Communication communication = new Communication(socket);
+                new Executor(communication).start();
+            }
+
+        } catch (IOException ex) {
+            logger.error(ex);
+        }
+    }
+}
